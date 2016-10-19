ffac96b1a5cf6070ccfe4fc4b905f7263e9e08c5
Dong Meng <dmeng@Dmeng-MB13.local>
2016-10-19 14:20:12 -0700

    file

--- src/main/java/com/mapr/examples/Producer.java
+++ src/main/java/com/mapr/examples/Producer.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileInputStream;
 import java.io.BufferedReader;
-import java.io.InputStreamReader
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 /**
